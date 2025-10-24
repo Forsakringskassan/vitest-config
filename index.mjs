f9dd9254f2c49dc625ff0300963ce5ce8e61fcc6
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-10-24 16:59:47 +0200

    fix: add typescript declarations

--- index.mjs
+++ index.mjs
@@ -4,7 +4,8 @@ export function defineTestConfig() {
     return {
         environment: "jsdom",
         coverage: {
-            provider,
+            /* eslint-disable-next-line object-shorthand -- technical debt: as long as the typecast is needed we cannot use shorthand */
+            provider: /** @type {"v8"} */ (/** @type {unknown} */ (provider)),
             reporter: ["text", "text-summary", "lcov"],
             include: ["src/**/*.[jt]s"],
             exclude: ["**/index.[jt]s"],
