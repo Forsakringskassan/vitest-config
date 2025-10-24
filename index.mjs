998857ab90bed9bb78bdc91ffcc627178b00b038
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-10-24 16:11:28 +0200

    fix: add coverage reporter

--- index.mjs
+++ index.mjs
@@ -1,8 +1,10 @@
+import provider from "@vitest/coverage-v8";
+
 export function defineTestConfig() {
     return {
         environment: "jsdom",
         coverage: {
-            provider: "v8",
+            provider,
             reporter: ["text", "text-summary", "lcov"],
             include: ["src/**/*.[jt]s"],
             exclude: ["**/index.[jt]s"],
