8104f3db2f71fbe6d90047cb510064a732e74d58
David Sveningsson <ext@sidvind.com>
2025-12-12 08:35:42 +0100

    style(eslint): fix object-shorthand warning

--- index.mjs
+++ index.mjs
@@ -4,7 +4,6 @@ export function defineTestConfig() {
     return {
         environment: "jsdom",
         coverage: {
-            /* eslint-disable-next-line object-shorthand -- technical debt: as long as the typecast is needed we cannot use shorthand */
             provider: /** @type {"v8"} */ (/** @type {unknown} */ (provider)),
             reporter: ["text", "text-summary", "lcov"],
             include: ["src/**/*.[jt]s"],
