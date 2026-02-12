965e5945763a5cca2b9201dc265b1480dc201d59
Olof Fredriksson <oloflolof@gmail.com>
2026-02-12 14:55:13 +0000

    feat: allow override

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import provider from "@vitest/coverage-v8";
+
+import { type TestUserConfig } from "vitest/config";
+import deepmerge from "deepmerge";
+
+function overwriteMerge<T>(_a: T[], b: T[]): T[] {
+    return b;
+}
+
+export function defineTestConfig(
+    userConfig: TestUserConfig = {},
+): TestUserConfig {
+    const defaultConfig: TestUserConfig = {
+        environment: "jsdom",
+        coverage: {
+            provider: provider as unknown as "v8",
+            reporter: ["text", "text-summary", "lcov"],
+            include: ["src/**/*.[jt]s"],
+            exclude: ["**/index.[jt]s"],
+        },
+    };
+
+    const resolvedConfig = userConfig
+        ? deepmerge(defaultConfig, userConfig, { arrayMerge: overwriteMerge })
+        : defaultConfig;
+    return resolvedConfig;
+}
