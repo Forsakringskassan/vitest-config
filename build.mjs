3a0a8d3127791c0d170bc70116c2ab922fabb16f
Olof Fredriksson <oloflolof@gmail.com>
2026-02-12 14:55:13 +0000

    feat: allow override

---
+++ build.mjs
@@ -0,0 +1,11 @@
+import { build } from "esbuild";
+
+await build({
+    entryPoints: ["./src/index.ts"],
+    outdir: "./dist",
+    minify: false,
+    bundle: true,
+    platform: "node",
+    format: "esm",
+    target: "node20",
+});
